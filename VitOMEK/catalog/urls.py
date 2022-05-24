3a0844037217072004b96ac482ee02ecd2dabcbb
senator23 <senator295@gmail.com>
2022-05-24 19:46:14 +0300

    comment urls & views

--- VitOMEK/catalog/urls.py
+++ VitOMEK/catalog/urls.py
@@ -5,7 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('premix/', views.Premix, name='premix'),
     path('sk1/', views.Sk1, name='sk1'),
-    # path('goods/', views.GoodsListView, name='goods'),
-    # path('animals/', views.AnimalListView, name='animals'),
 
 ]
--- VitOMEK/catalog/views.py
+++ VitOMEK/catalog/views.py
@@ -2,15 +2,9 @@ from django.shortcuts import render
 from .models import *
 
 def index(request):
-    # num_materials = TypeOfMaterials.objects.all().count()
-    # num_animals = Animal.objects.all().count()
-    # num_goods = Goods.objects.all().count()
-
-
     return render(request, 'index.html')
 
 def Premix(request):
-    # materials = Premix.objects.all()
     return render(request, 'premixes.html')
 
 def Sk1(request):
