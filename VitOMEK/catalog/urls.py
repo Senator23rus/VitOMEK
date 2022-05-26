26304dbf206acc4c94263e587dbc57eff2b1bf94
senator23 <senator295@gmail.com>
2022-05-26 10:15:11 +0300

    2 pages more. No DB + corrects

--- VitOMEK/catalog/urls.py
+++ VitOMEK/catalog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from catalog import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('premix/', views.Premix, name='premix'),
     path('sk1/', views.Sk1, name='sk1'),
-    # path('goods/', views.GoodsListView, name='goods'),
-    # path('animals/', views.AnimalListView, name='animals'),
+
 
 ]
--- VitOMEK/catalog/views.py
+++ VitOMEK/catalog/views.py
@@ -2,18 +2,10 @@ from django.shortcuts import render
 from .models import *
 
 def index(request):
-    num_materials = TypeOfMaterials.objects.all().count()
-    num_animals = Animal.objects.all().count()
-    num_goods = Goods.objects.all().count()
 
-
-    return render(request, 'index.html', context={'num_materials': num_materials,
-                                                  'num_animals': num_animals,
-                                                  'num_goods': num_goods,
-                                                  })
+    return render(request, 'index.html')
 
 def Premix(request):
-    # materials = Premix.objects.all()
     return render(request, 'premixes.html')
 
 def Sk1(request):
