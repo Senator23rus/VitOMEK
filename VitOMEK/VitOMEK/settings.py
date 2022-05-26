2fa91b90e3592ee96f5b0b642b80a4d448d6e704
senator23 <senator295@gmail.com>
2022-05-26 11:56:16 +0300

    password and password reset

--- VitOMEK/VitOMEK/settings.py
+++ VitOMEK/VitOMEK/settings.py
@@ -16,7 +16,6 @@ import os.path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -28,17 +27,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'catalog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalog',
+
 ]
 
 MIDDLEWARE = [
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'VitOMEK.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'VitOMEK.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -82,17 +80,16 @@ DATABASES = {
     # }
 
     # PostgreSQL
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'VitOMEK',
-            'USER': 'admin_vitomek',
-            'PASSWORD': '236450',
-            'HOST': 'localhost',
-            'PORT': '5432'
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'VitOMEK',
+        'USER': 'admin_vitomek',
+        'PASSWORD': '236450',
+        'HOST': 'localhost',
+        'PORT': '5432'
 
-        }
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -112,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -124,7 +120,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
@@ -132,10 +127,13 @@ STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [STATIC_DIR]
 
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- VitOMEK/catalog/urls.py
+++ VitOMEK/catalog/urls.py
@@ -1,11 +1,14 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from catalog import views
-
+from django.conf.urls import url
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('premix/', views.Premix, name='premix'),
     path('sk1/', views.Sk1, name='sk1'),
-
-
+]
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
